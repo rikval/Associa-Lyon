9142276f9785bdfaae824f4f1e3e0a8bf2bcd491
rikval <ericvwf3@gmail.com>
2019-02-28 15:48:31 +0100

    Add proposition crud

---
+++ src/Form/PropositionType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Proposition;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropositionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('created_at')
+            ->add('user')
+            ->add('categorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Proposition::class,
+        ]);
+    }
+}
