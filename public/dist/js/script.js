bf9c2460077d246fd27530650c4db549913b1614
Nadynely <leraynadine@hotmail.com>
2019-03-04 11:56:51 +0100

    Initiative templates update

--- public/dist/js/script.js
+++ public/dist/js/script.js
@@ -2,15 +2,7 @@
 
   $('#newsletter').on('submit', function () {
 
-      if ($('#firstname').val().length < 5) {
-          $('.error').slideDown('slow');
-          return false;
-
-      } else if ($('#lastname').val().length < 5) {
-          $('.error').slideDown('slow');
-          return false;
-
-      } else if ($('#email').val().length < 5) {
+      if ($('#email').val().length < 5) {
           $('.error').slideDown('slow');
           return false;
 
